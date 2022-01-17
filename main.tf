4e03641e7ce4a132cb26fa18f21076cb7ea0e680
PinkNoize <21967246+PinkNoize@users.noreply.github.com>
2022-01-17 14:36:28 -0800

    Adjusted member binding

--- main.tf
+++ main.tf
@@ -51,27 +51,27 @@ resource "google_service_account" "service_account" {
 resource "google_project_iam_member" "custom-role-iam" {
   project = var.project
   role    = google_project_iam_custom_role.command_func_role.id
-  members = ["serviceAccount:${google_service_account.service_account.email}"]
+  member = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "firestore-iam" {
   project = var.project
   role    = "roles/datastore.user"
-  members = ["serviceAccount:${google_service_account.service_account.email}"]
+  member = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "compute-iam" {
   project = var.project
   role    = "roles/compute.admin"
-  members = ["serviceAccount:${google_service_account.service_account.email}"]
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 # Add permissions to access DNS project
 resource "google_project_iam_member" "dns-iam" {
   provider = google.dns
-  project = var.dns_project_id
-  role    = "roles/server_manager_dns_role"
-  members = ["serviceAccount:${google_service_account.service_account.email}"]
+  project  = var.dns_project_id
+  role     = "roles/server_manager_dns_role"
+  member   = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 module "command_function" {
