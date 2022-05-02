c918a144442e7fac9bf680ee6c134cf93d34fbae
PinkNoize <21967246+PinkNoize@users.noreply.github.com>
2022-05-01 19:27:35 -0700

    Remove log prefix

--- discord-function/discord-function.go
+++ discord-function/discord-function.go
@@ -77,6 +77,10 @@ func (e LogEntry) String() string {
 
 func init() {
 	var err error
+	// Disable log prefixes such as the default timestamp.
+	// Prefix text prevents the message from being parsed as JSON.
+	// A timestamp is added when shipping logs to Cloud Logging.
+	log.SetFlags(0)
 	ctx := context.Background()
 	firestoreClient, err = firestore.NewClient(ctx, projectID)
 	if err != nil {
